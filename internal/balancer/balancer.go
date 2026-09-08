3ee1dd846ab3ff13691724cbc14e3bec022b434e
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2026-09-08 20:21:12 +0530

    feat: updated watch plan handler func in balancer.

--- internal/balancer/balancer.go
+++ internal/balancer/balancer.go
@@ -36,11 +36,62 @@ type BalancerState struct {
 	current int
 }
 
+func createServer(host string, port int) *Server {
+	return &Server{
+		Host:                 host,
+		Port:                 port,
+		ServerUtilization:    0.0,
+		InFlightRequestCount: 0,
+		RollingErrorRate:     0.0,
+	}
+}
+
 func createBalancer(addr string) *BalancerState {
 	return &BalancerState{
 		addr:    addr,
 		cluster: make([]*Server, 0),
 		current: 0,
+	}
+}
+
+func balancerWatchHandler(b *BalancerState) func(blockParam watch.BlockingParamVal, data interface{}) {
+	return func(blockParam watch.BlockingParamVal, data interface{}) {
+		if data == nil {
+			log.Println("[balancer]: no data received from consul watch")
+			return
+		}
+
+		services, ok := data.([]*capi.ServiceEntry)
+		if !ok {
+			log.Println("[balancer]: failed to cast data to []*capi.ServiceEntry")
+			return
+		}
+
+		newCluster := make([]*Server, 0)
+		for _, entry := range services {
+			status := "passing"
+			for _, check := range entry.Checks {
+				if check.Status != capi.HealthPassing {
+					status = "unhealthy"
+					break
+				}
+			}
+
+			if status == "passing" {
+				log.Printf("[balancer]: service %s is healthy", entry.Service.Service)
+				server := createServer(entry.Service.Address, entry.Service.Port)
+				newCluster = append(newCluster, server)
+			} else {
+				log.Printf("[balancer]: service %s is unhealthy", entry.Service.Service)
+
+			}
+		}
+
+		b.cluster = newCluster
+		for _, server := range b.cluster {
+			log.Printf("[balancer]: registered server %s:%d", server.Host, server.Port)
+		}
+
 	}
 }
 
@@ -79,25 +130,10 @@ func Start() {
 	plan, err := watch.Parse(watchConfig)
 	if err != nil {
 		log.Fatalf("failed to create watch plan: %v", err)
+		panic(err)
 	}
 
-	plan.Handler = func(idx uint64, data interface{}) {
-		services, ok := data.([]*capi.ServiceEntry)
-		if !ok {
-			log.Println("failed to cast data to []*capi.ServiceEntry")
-			return
-		}
-
-		var activeServers []string
-		for _, service := range services {
-			addr := service.Service.Address
-			port := service.Service.Port
-			activeServers = append(activeServers, fmt.Sprintf("%s:%d", addr, port))
-		}
-
-		log.Printf("[balancer]: active servers for service %s: %v", targetServiceName, activeServers)
-	}
-
+	plan.HybridHandler = balancerWatchHandler(balancer)
 	fmt.Println("[balancer]: starting consul watch for flux-backend service")
 
 	consulServerAddr := os.Getenv("CONSUL_HTTP_ADDR")
--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	capi "github.com/hashicorp/consul/api"
 )
@@ -28,22 +29,22 @@ func healthRequestHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func Start() {
+	serviceID := getEnv("SERVICE_ID", "flux-server-1")
+	serviceName := getEnv("SERVICE_NAME", "flux-backend")
+	serviceHost := getEnv("SERVICE_HOST", "flux-server-1")
+	servicePort := getEnv("SERVICE_PORT", "8080")
 
 	go func() {
-		log.Println("[server]: starting server on port 8080")
+		log.Printf("[server]: starting server on port %s", servicePort)
 
 		http.HandleFunc("/", requestHandler(8080))
 		http.HandleFunc("/health", healthRequestHandler)
 
-		err := http.ListenAndServe(":8080", nil)
+		err := http.ListenAndServe(fmt.Sprintf(":%s", servicePort), nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
-
-	serviceID := getEnv("SERVICE_ID", "flux-server-1")
-	serviceName := getEnv("SERVICE_NAME", "flux-backend")
-	serviceHost := getEnv("SERVICE_HOST", "flux-server-1")
 
 	config := capi.DefaultConfig()
 	client, err := capi.NewClient(config)
@@ -51,12 +52,18 @@ func Start() {
 		panic(err)
 	}
 
+	port, err := strconv.Atoi(servicePort)
+	if err != nil {
+		log.Fatalf("coversion of port failed: %v", err)
+		panic(err)
+	}
+
 	registration := &capi.AgentServiceRegistration{
 		ID:   serviceID,
 		Name: serviceName,
-		Port: 8080,
+		Port: port,
 		Check: &capi.AgentServiceCheck{
-			HTTP:                           fmt.Sprintf("http://%s:8080/health", serviceHost),
+			HTTP:                           fmt.Sprintf("http://%s:%s/health", serviceHost, servicePort),
 			Interval:                       "10s",
 			Timeout:                        "5s",
 			DeregisterCriticalServiceAfter: "1m",
